861f56fc3491e9c35234bd137ae62670ffe52637
Bablu yeshwanth <50798369+yeshwanth235@users.noreply.github.com>
2023-01-22 14:26:55 +0530

    adding spring boot project

---
+++ src/test/java/com/example/microservices/limitservice/LimitserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
